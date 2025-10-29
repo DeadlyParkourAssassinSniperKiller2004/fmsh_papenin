e62a05e376b5a53bf3f978329cdaa54e7f4144e7
DeadlyParkourAssassinSniperKiller2004 <papeninila05@gmail.com>
2025-10-29 09:56:33 +0700

    A(i believe last request)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <istream>
 
+
+
+std::pair <std::string, int> encrypt() {
+    int i;
+    std::vector<std::string> v;
+    std::cin >> v;
+    std::cin >> i;
+    return std::make_pair(v, i);
+}
+
+std::string decrypt() {
+
+    auto x = encrypt();
+    for (int a = 0; a < x.first.length(); a++) {
+        x.first[a] = x.first[a] + x.second;
+    }
+    return x.first;
+}
 int main() {
-    std::cout << decrypted();
+    std::cout << decrypt();
 }
